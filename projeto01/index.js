79cd374fbdae50e7dd3e5e7e6fccbb819404ad53
Ana Carolina <garcone.anaxx@gmail.com>
2021-08-21 23:51:32 -0300

    version 1.1

--- projeto01/index.js
+++ projeto01/index.js
@@ -48,6 +48,42 @@ app.get('/filmes/:id', (req, res) =>{
   res.send(filme);
 });
 
+app.post("/filmes", (req, res) => {
+  const filme = req.body.filme;
+  const id = filmes.length;
+  filmes.push(filme);
+
+  res.send(`Filme adicionado: ${filme}. 
+  O ID do filme é ${id}`);
+});
+
+
+app.put("/filmes/:id", (req, res) => {
+  const id = req.params.id - 1;
+  const filme = req.body.filme;
+  const nomeAnterior = filmes[id];
+
+  filmes[id] = filme;
+
+  res.send(
+    `Filme anterior: ${nomeAnterior}, atualizado para: ${filme}.`
+  );
+});
+
+
+app.delete("/filmes/:id", (req, res) => {
+  const id = req.params.id - 1;
+  const filme = filmes[id];
+  if (!filme) {
+    res.status(404).send({
+      message:"Filme não encontrado. Tente novamente."
+    });
+    return;
+  }
+  delete filmes[id];
+  res.send("Filme excluido com sucesso");
+});
+
 app.listen(port, () =>{
   console.info(`App rodando, ctrl + click: http://localhost:${port}/`);
 });
--- projeto02/index.js
+++ projeto02/index.js
@@ -58,6 +58,42 @@ app.get('/game-aleatorio', (req, res) =>{
   res.send(`<h1>Essa pagina gera um dos games de forma aleatoria:</h1> <p>${infoRandom(games,randomMinMax(0,games.length))}</p>`);
 });
 
+app.post('/games', (req, res) =>{
+  const game = req.body.game;
+  const id = games.length + 1;
+
+  games.push(game);
+  res.send(`Jogo adicionado com sucesso:${game}. O ID do game é ${id}`);
+});
+
+app.put('/games/:id', (req, res) =>{
+  const id = req.params.id -1;
+  const game = req.body.game;
+  const nomeAnterior = games[id];
+
+  games[id] = game;
+  res.send(`Jogo anterior: ${nomeAnterior}, atualizado para: ${game}`);
+});
+
+// app.delete('/games/:id', (req, res) =>{
+//   const id = req.params.id - 1;
+
+//   if (id > games.length || id < 0) {
+//     res.send('Jogo nao encontrado. Tente novamente com outro ID.');
+//   } else {
+//     delete games[id];
+//     res.send('Jogo excluido com sucesso!');
+//   }
+  
+// });
+
+app.delete('/games/:id', (req, res)=>{
+  const id = req.params.id - 1;
+  games.splice(id, 1)
+  res.send("Jogo excluido com sucesso.")
+  // colocar verificacao
+});
+
 app.listen(port, () =>{
   console.info(`App rodando, ctrl + click: http://localhost:${port}/`);
 });
